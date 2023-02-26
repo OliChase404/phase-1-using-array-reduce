aeaf0318103ae7cc0b2a2ad917de2e5d047086da
Oliver <olichase91@gmail.com>
2023-02-25 19:15:25 -0800

    Solved Lab

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((a, i) => a + i, 0)
+
+console.log(totalBatteries)
+
+
+
+
+
+
+
+
+
+
+
+
+const products = [
+    { name: 'Shampoo', price: 4.99 },
+    { name: 'Donuts', price: 7.99 },
+    { name: 'Cookies', price: 6.49 },
+    { name: 'Bath Gel', price: 13.99 }
+  ];
+  
+  function getTotalAmountForProducts(products) {
+    let totalPrice = 0;
+  
+    for (const product of products) {
+      totalPrice += product.price;
+    };
+  
+    return totalPrice;
+  }
+  
+//   console.log(getTotalAmountForProducts(products)); 
+//   console.log(products.reduce((x,y) => products[i].price,))
+
+  console.log(products.reduce((accumulator, i) => accumulator + i.price, 0))
+
